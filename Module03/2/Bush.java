2ed30a500f3a9ae30a800f0c1db3eb688be47089
sirxeed <sir.xeed@gmail.com>
2015-12-19 18:52:57 +0200

    Create Bush.java

---
+++ Module03/2/Bush.java
@@ -0,0 +1,19 @@
+public class Bush {
+    //Поля класу
+    Rose[] roses;
+    int quantity;
+
+    //Конструктори
+    Bush() {
+        quantity = 50;
+    }
+
+    //Методи
+    public void cut() {
+        //Cut algorithm
+    }
+
+    public void Smell() {
+        //Cut algorithm
+    }
+}
