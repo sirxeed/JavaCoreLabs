0eb758569ad3175b1a95fed6ef6fb2505085a931
sirxeed <sir.xeed@gmail.com>
2015-12-20 00:07:38 +0200

    Update PointDistance.java

--- Module04/PointDistance.java
+++ Module04/PointDistance.java
@@ -2,6 +2,15 @@ public class PointDistance {
     public static float distance (int x1, int y1, int x2, int y2) {
         int deltaX = x2 - x1;
         int deltaY = y2 - y1;
+        
+        //negative distance check
+        if (deltaX < 0) {
+            deltaX *= -1;
+        }
+        if (deltaY < 0) {
+            deltaY *= -1;
+        }
+        
         return (float) Math.pow(Math.pow(deltaX, 2) + Math.pow(deltaY, 2), 0.5);
     }
 }
