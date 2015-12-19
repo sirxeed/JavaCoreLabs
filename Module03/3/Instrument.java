bc293a623ee936fe6f3ecfab2365502f296a4e5e
sirxeed <sir.xeed@gmail.com>
2015-12-19 18:59:30 +0200

    Create Instrument.java

---
+++ Module03/3/Instrument.java
@@ -0,0 +1,20 @@
+public class Instrument {
+    //Поля класу
+    public String type;
+    public String brand;
+    public String model;
+    public int color;
+    
+    //Конструктор
+    Instrument(String type, String brand, String model, int color) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+    
+    //Методи
+    public void play() {
+        //play algorithm
+    }
+}
