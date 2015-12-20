8f7c7f727fac13c46c9ea3b202865fdee8b9f3d2
sirxeed <sir.xeed@gmail.com>
2015-12-20 23:42:44 +0200

    Update Aster.java

--- Module03/2/Aster.java
+++ Module03/2/Aster.java
@@ -1,5 +1,13 @@
 public class Aster extends Flower {
-    public String variety;
+    private String variety;
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
 
     //Якась логіка
 }
