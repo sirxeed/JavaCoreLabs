b1a4f31ff313399a8c460709aafb39ca5fc869bc
sirxeed <sir.xeed@gmail.com>
2015-12-20 23:49:11 +0200

    Update GraphicFile.java

--- Module03/1/GraphicFile.java
+++ Module03/1/GraphicFile.java
@@ -1,5 +1,22 @@
 public class GraphicFile extends File {
     //Поля класу
-    public String contents; //Нехай буде стрінг
-    public String colorIndex;
+    private String contents; //Нехай буде стрінг
+    private String colorIndex;
+
+    //Методи класу
+    public String getContents () {
+        return contents;
+    }
+
+    public void setContents (String contents) {
+        this.contents = contents;
+    }
+
+    public String getColorIndex () {
+        return colorIndex;
+    }
+
+    public void setColorIndex (String colorIndex) {
+        this.colorIndex = colorIndex;
+    }
 }
