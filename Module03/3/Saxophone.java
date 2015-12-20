026c5bd5ad57c0fa04bf9b173021182df6a63561
sirxeed <sir.xeed@gmail.com>
2015-12-20 23:42:01 +0200

    Update Saxophone.java

--- Module03/3/Saxophone.java
+++ Module03/3/Saxophone.java
@@ -1,6 +1,15 @@
 public class Saxophone extends Instrument {
     //Поля класу
-    public String metal;
+    private String metal;
 
+    //Методи
+    //Геттери і сеттери
+    public String getMetal() {
+        return metal;
+    }
+
+    public void setMetal(String metal) {
+        this.metal = metal;
+    }
     //Щось тут ще
 }
