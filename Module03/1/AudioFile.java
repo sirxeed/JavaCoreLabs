e00fc6703e94d77a2bd54dd9a579ce8c87e60eac
sirxeed <sir.xeed@gmail.com>
2015-12-20 23:45:35 +0200

    Update AudioFile.java

--- Module03/1/AudioFile.java
+++ Module03/1/AudioFile.java
@@ -2,4 +2,21 @@ public class AudioFile extends File {
     //Поля класу
     public String contents; //Нехай буде стрінг
     public int frequency;
+
+    //Методи класу
+    public String getContents () {
+        return contents;
+    }
+
+    public void setContents (String contents) {
+        this.contents = contents;
+    }
+
+    public int getFrequency () {
+        return frequency;
+    }
+
+    public void setFrequency (int frequency) {
+        this.frequency = frequency;
+    }
 }
