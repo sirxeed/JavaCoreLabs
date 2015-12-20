03f5f1c26bdb16cdfafb52101ddc47e730883e5d
sirxeed <sir.xeed@gmail.com>
2015-12-21 00:12:31 +0200

    Update Directory.java

--- Module03/1/Directory.java
+++ Module03/1/Directory.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class Directory extends File {
-    //Згідно UML-діаграми тут, в принципі, і розширювати нічого :-)
+    /*
+    //ArrayList<File> files = new ArrayList<File>();
+    //files.add(new GraphicFile);
+    */
+
+    File[] files = new File[100];
+
+    files[0] = new AudioFile;
+
+    for (int i = 1; i <= 10; i++) {
+        files[i] = new GraphicFile();
+    }
 }
