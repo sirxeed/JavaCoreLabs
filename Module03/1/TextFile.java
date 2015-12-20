99ac7e37d460582474075ff647be793193f4209a
sirxeed <sir.xeed@gmail.com>
2015-12-20 23:49:26 +0200

    Update TextFile.java

--- Module03/1/TextFile.java
+++ Module03/1/TextFile.java
@@ -1,6 +1,31 @@
 public class TextFile extends File {
     //Поля класу
-    public String contents; //Нехай буде стрінг
-    public String author;
-    public String encoding;
+    private String contents; //Нехай буде стрінг
+    private String author;
+    private String encoding;
+
+    //Методи класу
+    public String getContents () {
+        return contents;
+    }
+
+    public void setContents (String contents) {
+        this.contents = contents;
+    }
+
+    public String getAuthor () {
+        return author;
+    }
+
+    public void setAuthor (String author) {
+        this.author = author;
+    }
+
+    public String getEncoding () {
+        return encoding;
+    }
+
+    public void setEncoding (String encoding) {
+        this.encoding = encoding;
+    }
 }
