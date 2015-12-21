0c72351b1b1bc402a35c7d255ea4ca0350085cbe
sirxeed <sir.xeed@gmail.com>
2015-12-21 22:01:11 +0200

    Update Directory.java

--- Module03/1/Directory.java
+++ Module03/1/Directory.java
@@ -3,5 +3,18 @@ package package1;
 import java.util.ArrayList;
 
 public class Directory extends File {
-    ArrayList<File> files = new ArrayList<File>();
+    private ArrayList<File> files = new ArrayList<File>();
+
+    Directory(ArrayList<File> files) {
+
+    }
+
+    //Геттер
+    public ArrayList<File> getFiles() {
+        return files;
+    }
+
+    public void addFiles(File file) {
+        files.add(file);
+    }
 }
