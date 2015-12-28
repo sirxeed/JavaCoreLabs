70a9874d7f53f53e843b4e7ec269af0d645828d6
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:35:13 +0200

    Create Flower.java

---
+++ Module06/package2/Flower.java
@@ -0,0 +1,45 @@
+package package2;
+
+public class Flower {
+    //Поля класу
+    private int height;
+    private int color;
+
+    //Конструктори
+    Flower() {
+        height = 10;
+        color = 0xff0000;
+    }
+
+    Flower(int height, int color) {
+        this.height = height;
+        this.color = color;
+    }
+
+    //Методи класу
+    //Геттери і сеттери
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    //Логіка
+    public void cut() {
+        //Cut algorithm
+    }
+
+    public void smell() {
+        //Smell algorithm
+    }
+}
