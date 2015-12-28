6b7285aca5629aeac60b85c9df1c92bc66cd4f18
sirxeed <sir.xeed@gmail.com>
2015-12-29 00:45:40 +0200

    Update Start.java

--- Module06/package1/Start.java
+++ Module06/package1/Start.java
@@ -1,18 +1,33 @@
 package package1;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Start {
-    public static void main(String[] args) {
-        ArrayList<File> files = new ArrayList<File>();
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<File> files = new ArrayList<>();
         Directory directory = new Directory(files);
 
-        directory.addFiles(new GraphicFile());
-        for (int i = 1; i <= 10; i++) {
-            directory.addFiles(new AudioFile());
-        }
-        directory.addFiles(new TextFile());
-        directory.addFiles(new TextFile());
+        Scanner scanner = new Scanner(System.in);
+        directory.setName("dir1");
+        System.out.println("You created a directory '" + directory.getName() + "'");
 
+        directory.addFiles(new TextFile());
+        directory.getFiles().get(0).setName("File1");
+        System.out.println("'" + directory.getFiles().get(0).getName() + "' file has been created");
+
+        directory.addFiles(new TextFile());
+        directory.getFiles().get(1).setName("File2");
+        System.out.println("'" + directory.getFiles().get(1).getName() + "' file has been created");
+
+        directory.addFiles(new TextFile());
+        directory.getFiles().get(2).setName("File3");
+        System.out.println("'" + directory.getFiles().get(2).getName() + "' file has been created");
+
+        //Візьмемо файл за іменем
+        System.out.print("Enter the file name you want to get: ");
+        String fileName = scanner.next();
+        directory.getFileByName(fileName);
     }
 }
