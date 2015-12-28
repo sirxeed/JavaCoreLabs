d866eb15bb03bea7ebc0892f7dc440c432d97486
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:34:21 +0200

    Create Aster.java

---
+++ Module06/package2/Aster.java
@@ -0,0 +1,15 @@
+package package2;
+
+public class Aster extends Flower {
+    private String variety;
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    //Якась логіка
+}
