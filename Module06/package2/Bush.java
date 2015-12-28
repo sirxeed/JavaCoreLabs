b2866da9a10b0e903ad024e65c291447770e2115
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:34:46 +0200

    Create Bush.java

---
+++ Module06/package2/Bush.java
@@ -0,0 +1,21 @@
+package package2;
+
+public class Bush {
+    //Поля класу
+    private Rose[] roses = new Rose[100];
+    int quantity;
+
+    //Конструктори
+    Bush() {
+        quantity = 50;
+    }
+
+    //Методи
+    public void cut() {
+        //Cut algorithm
+    }
+
+    public void smell() {
+        //Cut algorithm
+    }
+}
