5143b90acdc9dff6ea6b7d85bd87917652060442
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:36:46 +0200

    Create Instrument.java

---
+++ Module06/package3/Instrument.java
@@ -0,0 +1,61 @@
+package package3;
+
+public class Instrument {
+    //Поля класу
+    private String type;
+    private String brand;
+    private String model;
+    private int color;
+
+    //Конструктор
+    Instrument() {
+        type = "guitar";
+        brand = "Gibson";
+        model = "Les Paul";
+        color = 0x903000;
+    }
+
+    Instrument(String type, String brand, String model, int color) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    //Методи
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public void play() {
+        //play algorithm
+    }
+}
