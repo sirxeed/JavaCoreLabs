43a4f5dc4880991593417c3f77d0b9a2a08d7041
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:37:20 +0200

    Create Piano.java

---
+++ Module06/package3/Piano.java
@@ -0,0 +1,19 @@
+package package3;
+
+public class Piano extends Instrument {
+    //Поля класу
+    private String keys;
+
+    //Методи
+    //Геттери і сеттери
+    public String getKeys() {
+        return keys;
+    }
+
+    public void setStings(String keys) {
+        this.keys = keys;
+    }
+
+    //Щось тут ще
+
+}
