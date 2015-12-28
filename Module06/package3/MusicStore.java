00116210099a5999c96b0a3fd51876e9074eb118
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:36:58 +0200

    Create MusicStore.java

---
+++ Module06/package3/MusicStore.java
@@ -0,0 +1,19 @@
+package package3;
+
+public class MusicStore {
+    //Поля класу
+    private Instrument[] instruments = new Instrument[100];
+    private String address;
+
+    //Методи
+    //Геттери і сеттери
+    public String getStings() {
+        return address;
+    }
+
+    public Instrument[] getInstruments() {
+        return instruments;
+    }
+
+    //Щось тут ще
+}
