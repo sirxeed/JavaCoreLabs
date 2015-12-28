fb15313ee0dd99bceee03a488a08fd10ceca3eaf
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:32:42 +0200

    Create AudioFile.java

---
+++ Module06/package1/AudioFile.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class AudioFile extends File {
+    //Поля класу
+    public String contents; //Нехай буде стрінг
+    public int frequency;
+
+    //Методи класу
+    public String getContents () {
+        return contents;
+    }
+
+    public void setContents (String contents) {
+        this.contents = contents;
+    }
+
+    public int getFrequency () {
+        return frequency;
+    }
+
+    public void setFrequency (int frequency) {
+        this.frequency = frequency;
+    }
+}
