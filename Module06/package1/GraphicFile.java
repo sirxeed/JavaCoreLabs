99c3782caa503aa60d6840451711f02cb9c0f3c9
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:33:21 +0200

    Create GraphicFile.java

---
+++ Module06/package1/GraphicFile.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class GraphicFile extends File {
+    //Поля класу
+    private String contents; //Нехай буде стрінг
+    private String colorIndex;
+
+    //Методи класу
+    public String getContents () {
+        return contents;
+    }
+
+    public void setContents (String contents) {
+        this.contents = contents;
+    }
+
+    public String getColorIndex () {
+        return colorIndex;
+    }
+
+    public void setColorIndex (String colorIndex) {
+        this.colorIndex = colorIndex;
+    }
+}
