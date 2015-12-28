ee63c04c8b573625d6d8545f77077caa2051f302
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:33:58 +0200

    Create TextFile.java

---
+++ Module06/package1/TextFile.java
@@ -0,0 +1,33 @@
+package package1;
+
+public class TextFile extends File {
+    //Поля класу
+    private String contents; //Нехай буде стрінг
+    private String author;
+    private String encoding;
+
+    //Методи класу
+    public String getContents () {
+        return contents;
+    }
+
+    public void setContents (String contents) {
+        this.contents = contents;
+    }
+
+    public String getAuthor () {
+        return author;
+    }
+
+    public void setAuthor (String author) {
+        this.author = author;
+    }
+
+    public String getEncoding () {
+        return encoding;
+    }
+
+    public void setEncoding (String encoding) {
+        this.encoding = encoding;
+    }
+}
