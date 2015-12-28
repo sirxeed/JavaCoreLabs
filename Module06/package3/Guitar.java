779389245aa082b459fff73848764bb5ff314407
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:36:32 +0200

    Create Guitar.java

---
+++ Module06/package3/Guitar.java
@@ -0,0 +1,18 @@
+package package3;
+
+public class Guitar extends Instrument {
+    //Поля класу
+    private String stings;
+
+    //Методи
+    //Геттери і сеттери
+    public String getStings() {
+        return stings;
+    }
+
+    public void setStings(String stings) {
+        this.stings = stings;
+    }
+
+    //Щось тут ще
+}
