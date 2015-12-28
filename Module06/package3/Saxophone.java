7e777c57fd17178ff4683e63cbdf82c12dfece7f
sirxeed <sir.xeed@gmail.com>
2015-12-28 23:37:32 +0200

    Create Saxophone.java

---
+++ Module06/package3/Saxophone.java
@@ -0,0 +1,17 @@
+package package3;
+
+public class Saxophone extends Instrument {
+    //Поля класу
+    private String metal;
+
+    //Методи
+    //Геттери і сеттери
+    public String getMetal() {
+        return metal;
+    }
+
+    public void setMetal(String metal) {
+        this.metal = metal;
+    }
+    //Щось тут ще
+}
